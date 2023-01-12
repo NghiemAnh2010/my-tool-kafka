f7d2d715f5bf1268ee8ff9501d7a8b484ccb2b4b
Anh <anhnbq@AM.LOCAL>
2023-01-12 18:14:04 +0700

    MTK-1-update-limit-message

--- src/main/java/com/example/mytoolkafka/configuration/KafkaConsumerConfig.java
+++ src/main/java/com/example/mytoolkafka/configuration/KafkaConsumerConfig.java
@@ -35,6 +35,10 @@ public class KafkaConsumerConfig {
         props.put(ConsumerConfig.GROUP_ID_CONFIG,"group-consumer-login-1");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        // Số message tối đa được trả về trong 1 lần gọi hàn poll()
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG,1000);
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG,true);
+
 
         return new DefaultKafkaConsumerFactory<>(props);
     }
